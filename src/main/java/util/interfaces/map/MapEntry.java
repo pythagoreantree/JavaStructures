58b4cfb0f1683868737af897c7d42cfd4aa04ec1
Maria Teplinskaya <mteplinskaya@cs.ad.1worldsync.com>
2022-04-27 01:39:58 +0300

    Commit

--- src/main/java/util/interfaces/map/MapEntry.java
+++ src/main/java/util/interfaces/map/MapEntry.java
@@ -1,4 +1,14 @@
 package util.interfaces.map;
 
 public interface MapEntry<K,V> {
+
+    K getKey();
+
+    V getValue();
+
+    V setValue(V value);
+
+    boolean equals(Object o);
+
+    int hashCode();
 }
