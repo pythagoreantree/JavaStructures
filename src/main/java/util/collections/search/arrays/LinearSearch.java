36a49e3b6aa4262c2097bcb87f37d9f3ab55458c
Maria Teplinskaya <mteplinskaya@cs.ad.1worldsync.com>
2022-05-24 15:51:12 +0300

    //features

--- src/main/java/util/collections/search/arrays/LinearSearch.java
+++ src/main/java/util/collections/search/arrays/LinearSearch.java
@@ -5,4 +5,8 @@ public class LinearSearch implements Search {
     public <E> int search(E[] arr, E elem) {
         return 0;
     }
+
+    public <E> int searchEdge(E[] arr, E elem) {
+        return 0;
+    }
 }
