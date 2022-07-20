b5d05290e0ce901da19f03848f0657bfa6b6feb8
Maria Teplinskaya <pythagoreantree@googlemail.com>
2022-07-20 23:50:07 +0300

    + Red-Black Tree first part of insertFixUp (foooouhh)

--- src/main/java/util/collections/implementations/trees/RedBlackTree.java
+++ src/main/java/util/collections/implementations/trees/RedBlackTree.java
@@ -6,6 +6,9 @@ public class RedBlackTree<T> extends BinarySearchTree<T> {
 
     public RedBlackTree() {}
 
+    /*
+    * O(h), h <= 2log(n+1)
+    * */
     public void add(T key){
         TreeNode<T> newNode = new TreeNode<>(key, endNode, endNode);
         TreeNode<T> nodeParent = endNode;
@@ -20,6 +23,7 @@ public class RedBlackTree<T> extends BinarySearchTree<T> {
         newNode.parent = nodeParent;
         if (nodeParent == endNode) {
             root = newNode;
+            return;
         } else if (newkey.compareTo(newNode.key) < 0){
             nodeParent.left = newNode;
         } else {
@@ -30,6 +34,68 @@ public class RedBlackTree<T> extends BinarySearchTree<T> {
     }
 
     private void insertFixup(TreeNode<T> node) {
+        //what if parent is black?
+        if (node == null)
+            throw new NullPointerException("Input Node is null.");
+        //what if node == endNode
+        TreeNode<T> parent = node.parent;
+        if (parent == null)
+            throw new NullPointerException("Parent node is null.");
+        //what if parent == endNode
 
+        while (parent.color == 1) {
+            //does GP exists?
+            if (parent == parent.parent.left){
+                TreeNode<T> uncle = parent.parent.right;
+                //if uncle == null
+                if (uncle == null || uncle.color == 0) {
+                    if (node == node.parent.right){
+                        parent.parent.left = leftRotate(node.parent);
+                    }
+                    swapColors(parent.parent, parent.parent.left);
+                    TreeNode<T> rotatedGP = rightRotate(parent.parent);
+                    if (parent.parent == parent.parent.left)
+                        parent.parent.left = rotatedGP;
+                    else
+                        parent.parent.right = rotatedGP;
+                } else if (uncle != null && uncle.color == 1) {
+                    parent.color = 0;
+                    uncle.color = 0;
+                    parent.parent.color = 1;
+                    node = node.parent.parent;
+                }
+            } else if (parent == parent.parent.right){
+
+            } else {
+                throw new RuntimeException("Smth wrong with P - GP linkage.");
+            }
+        }
+        //here root.color = 0;
+    }
+
+    private void swapColors(TreeNode<T> node1, TreeNode<T> node2) {
+        byte color = node1.color;
+        node1.color = node2.color;
+        node2.color = color;
+    }
+
+    private TreeNode<T> leftRotate(TreeNode<T> z) {
+        TreeNode<T> y = z.right;
+        TreeNode<T> T2 = y.left;
+
+        y.left = z;
+        z.right = T2;
+
+        return y;
+    }
+
+    private TreeNode<T> rightRotate(TreeNode<T> z) {
+        TreeNode<T> y = z.left;
+        TreeNode<T> T2 = y.right;
+
+        y.right = z;
+        z.left = T2;
+
+        return y;
     }
 }
